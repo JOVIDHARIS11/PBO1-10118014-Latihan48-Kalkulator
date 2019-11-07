103eef443155a2a01f064ab079619b08e807ca5f
JOVIDHARIS11 <55835564+JOVIDHARIS11@users.noreply.github.com>
2019-11-07 21:11:19 +0700

    Add files via upload

---
+++ PBO1-10118014-Latihan48-Kalkulator/src/pbo1/pkg10118014/latihan48/kalkulator/PBO110118014Latihan48Kalkulator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118014.latihan48.kalkulator;
+
+/**
+ *
+ * @author 
+ * NAMA  : Jovi D Haris
+ * KELAS : IF 1
+ * NIM   : 10118014
+ * Deskripsi Program : Program ini untuk membuat operasi kalkulator.
+ */
+public class PBO110118014Latihan48Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Kalkulator objKal = new Kalkulator();
+        objKal.setValue1(7.0);
+        objKal.setValue2(5.0);
+        System.out.println("VALUE 1 = "+objKal.getValue1());
+        System.out.println("VALUE 2 = "+objKal.getValue2());
+        objKal.setNameProject();
+        objKal.setNoteProject("");
+        System.out.println("Result Add is = "+objKal.add());
+        System.out.println("Result Minus is = "+objKal.minus());
+        System.out.println("Result Multiple is = "+objKal.multiplication());
+        System.out.println("Result Division is = "+objKal.division());
+    }
+    
+}
